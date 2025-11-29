9f393d63795e0b2a28e73661b839f38b15397232
Hongbo Ma <154826089+Ma-Hongbo@users.noreply.github.com>
2025-11-30 00:20:32 +0800

    Set OPENBLAS_NUM_THREADS to 4 in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+export OPENBLAS_NUM_THREADS=4
 source /lustre/fs12/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.sh
 conda activate pycolmap
 python /lustre/fsw/portfolios/nvr/users/ymingli/projects/mhb/citygaussian/colmap/python/examples/panorama_sfm.py --input_image_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket_middle/sample_3 --output_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/supermarket_middle/sample_3_process --matcher vocabtree
